71763616fe3c783d925754a4e97704e2c52504ca
DragonMasterDyne <turbo675@gmail.com>
2017-11-01 13:23:11 -0600

    auth kinda

--- src/server/index.js
+++ src/server/index.js
@@ -14,9 +14,48 @@ app.use(session({
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+massive(process.env.CONNECTION_STRING).then( db =>{
+    app.set('db', db)
+})
+
+
+// Auth0 setup
+
+passport.use(new Auth0Strategy({
+    domain: process.env.AUTH_DOMAIN,
+    clientID: process.env.AUTH_CLIENTID,
+    clientSecret: process.env.AUTH_CLIENT_SECRET,
+    callbackURL: process.env.AUTH_CALLBACK 
+}, function(accessToken, refreshToken, extraParams, profile, done){
+    const userData = profile._json;
+    const db = app.get('db');
+    db.check_user([userData.email]).then( user => {
+        return done(null, user[0].email)
+    }) 
+}))
+passport.serializeUser(function(email, done){
+    done(null, email)
+})
+passport.deserializeUser(function(email, done){
+    const db = app.get('db');
+    db.find_session([email]).then(user => {
+    done(null, user[0])
+    })
+})
+
+// Login Endpoints
+
+app.get('/auth', passport.authenticate('auth0'));
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:3000/#/dashboard',
+    failureRedirect: '/auth'
 }))
 
 
 
-const PORT = 3535
-app.listen(PORT, () => console.log(`Listing on port: ${PORT}`))
+const PORT = 3535;
+app.listen(PORT, () => console.log(`Listing on port: ${PORT}`));
